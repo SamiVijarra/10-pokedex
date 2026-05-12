3be9b734a544b28955594acb212192b1db52b226
Sami <samivijarra1@gmail.com>
2026-05-11 22:54:33 -0300

    feat: customPipes - parseMongoIdPipe

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -11,6 +11,7 @@ import {
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -40,7 +41,7 @@ export class PokemonController {
     return this.pokemonService.update(term, updatePokemonDto);
   }
 
-  @Delete(':id')
+  @Delete(':id', ParseMongoIdPipe)
   remove(@Param('id') id: string) {
     return this.pokemonService.remove(id);
   }
